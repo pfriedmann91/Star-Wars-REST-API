d5a830d49310334e42e4479397a7a6db516e25fe
Pablo Friedmann <pabloifm91@gmail.com>
2025-03-11 13:05:03 +0000

    Final Commit/migraciones solucionadas/Users/user

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User, Planets, Characters, Vehicles, Spaceships, FavoriteCharacters, FavoritePlanets, FavoriteVehicles, FavoriteSpaceships 
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -11,6 +11,14 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Planets, db.session))
+    admin.add_view(ModelView(Characters, db.session))
+    admin.add_view(ModelView(Vehicles, db.session))
+    admin.add_view(ModelView(Spaceships, db.session))
+    admin.add_view(ModelView(FavoriteCharacters, db.session))
+    admin.add_view(ModelView(FavoritePlanets, db.session))
+    admin.add_view(ModelView(FavoriteVehicles, db.session))
+    admin.add_view(ModelView(FavoriteSpaceships, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -1,46 +1,155 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import mapped_column
-from sqlalchemy import Integer, String, Boolean
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, Mapped
+from typing import List
 
 db = SQLAlchemy()
 
-
 class User(db.Model):
     __tablename__ = "user"
-
     id = mapped_column(Integer, primary_key=True)
-    email = mapped_column(String(120), nullable=False)
-    password = mapped_column(String(80))
-    is_active = mapped_column(Boolean)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    name = mapped_column(String(50), nullable=True)
+    email = mapped_column(String(100), nullable=False)
+    password = mapped_column(String(100), nullable=False) 
+    
+    favorite_characters: Mapped[List["FavoriteCharacters"]] = relationship(back_populates="user")
+    favorite_planets: Mapped[List["FavoritePlanets"]] = relationship(back_populates="user")
+    favorite_vehicles: Mapped[List["FavoriteVehicles"]] = relationship(back_populates="user")
+    favorite_spaceships: Mapped[List["FavoriteSpaceships"]] = relationship(back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "email": self.email
             # do not serialize the password, its a security breach
         }
 
+class Planets(db.Model):
+    __tablename__ = "planets"
+    id = mapped_column(Integer, primary_key=True)
+    planet_name = mapped_column(String(50), nullable=False)
+    population = mapped_column(String(20), nullable=False)
+    area = mapped_column(String(10), nullable=False)
+    
+    favorite_planets: Mapped[List["FavoritePlanets"]] = relationship(back_populates="planet")
 
+    def serialize(self):
+        return {
+            "id_planet": self.id,
+            "planet_name": self.planet_name
+        }
 
-# from flask_sqlalchemy import SQLAlchemy
+class Characters(db.Model):
+    __tablename__ = "characters"
+    id = mapped_column(Integer, primary_key=True)
+    character_name = mapped_column(String(50), nullable=False)
+    eye_color = mapped_column(String(100), nullable=False)
+    hair_color = mapped_column(String(20), nullable=False)
+    height = mapped_column(String(10), nullable=False)
+    
+    favorite_characters: Mapped[List["FavoriteCharacters"]] = relationship(back_populates="character")
 
-# db = SQLAlchemy()
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character_name": self.character_name
+        }
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     password = db.Column(db.String(80), unique=False, nullable=False)
-#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class Vehicles(db.Model):
+    __tablename__ = "vehicles"
+    id = mapped_column(Integer, primary_key=True)
+    vehicle_name = mapped_column(String(50), nullable=False)
+    model = mapped_column(String(50), nullable=False)
+    passengers = mapped_column(String(10), nullable=False)
 
-#     def __repr__(self):
-#         return '<User %r>' % self.username
+    favorite_vehicles: Mapped[List["FavoriteVehicles"]] = relationship(back_populates="vehicle")
 
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "email": self.email,
-#             # do not serialize the password, its a security breach
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "vehicle_name": self.vehicle_name,
+            "model": self.model,
+            "passengers": self.passengers
+        }
+
+class Spaceships(db.Model):
+    __tablename__ = "spaceships"
+    id = mapped_column(Integer, primary_key=True)
+    spaceship_name = mapped_column(String(50), nullable=False)
+    model = mapped_column(String(50), nullable=False)
+    passengers = mapped_column(String(10), nullable=False)
+    favorite_spaceships: Mapped[List["FavoriteSpaceships"]] = relationship(back_populates="spaceship")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "spaceship_name": self.spaceship_name,
+            "model": self.model,
+            "passengers": self.passengers
+
+        }
+
+class FavoriteCharacters(db.Model):
+    __tablename__ = "favorite_characters"
+    id = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    character_id: Mapped[int] = mapped_column(ForeignKey("characters.id"))
+
+    user = relationship("User", back_populates="favorite_characters")
+    character = relationship("Characters", back_populates="favorite_characters")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id
+        }
+
+class FavoritePlanets(db.Model):
+    __tablename__ = "favorite_planets"
+    id = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
+
+    user = relationship("User", back_populates="favorite_planets")
+    planet = relationship("Planets", back_populates="favorite_planets")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id
+        }
+
+class FavoriteVehicles(db.Model):
+    __tablename__ = "favorite_vehicles"
+    id = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    vehicle_id: Mapped[int] = mapped_column(ForeignKey("vehicles.id"))
+
+    user = relationship("User", back_populates="favorite_vehicles")
+    vehicle = relationship("Vehicles", back_populates="favorite_vehicles")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "vehicle_id": self.vehicle_id
+        }
+
+class FavoriteSpaceships(db.Model):
+    __tablename__ = "favorite_spaceships"
+    id = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    spaceship_id: Mapped[int] = mapped_column(ForeignKey("spaceships.id"))
+
+    user = relationship("User", back_populates="favorite_spaceships")
+    spaceship = relationship("Spaceships", back_populates="favorite_spaceships")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "spaceship_id": self.spaceship_id
+        }
